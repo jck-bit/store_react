a9d8f0cd1b3b9251b6c090dac1ee9f1abd52304b
jck-bit <jckkinyanjui@gmail.com>
2021-10-23 21:04:03 +0300

    added the usesate to the store-manager

--- store_react/src/pages/Body.js
+++ store_react/src/pages/Body.js
@@ -1,20 +1,51 @@
+import React,{ useState } from 'react';
 import classes from './Body.module.css'
-import { Link } from 'react-router-dom'
+
 
 const Body = () => {
+    const [values, setValues] = useState({
+        username:"",
+        email:"",
+        password:""
+    })
+    const handleChange =(event) =>{
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value,
+        })
+     }
+
+
+    const handleFormsubmit =(event) =>{
+      event.preventDefault();
+    }
     return(
     <div className={classes.alot}>
       <h1>The store</h1>
         <form className={classes.wrapper}>
         <div className={classes.control}>
-            <input type="text" placeholder='Email' />
+            <input type="text" placeholder='username'
+             name="username" 
+             value={values.username} 
+             onChange={handleChange}
+             />
         </div>
         <div className={classes.control}>
-            <input type="text" placeholder='password' />
+            <input type="text"
+             placeholder='email' 
+             name="email" value={values.email}
+              onChange={handleChange}
+              />
         </div>
-        <Link to ='/Home'>
-        <button>Log in</button>
-        </Link>
+        <div className={classes.control}>
+            <input type="text" placeholder='password' 
+            name="password" 
+            value ={values.password} 
+            onChange={handleChange} 
+            />
+        </div>
+        <button onClick={handleFormsubmit}>Log in</button>
+ 
        </form>
     </div>
     )
